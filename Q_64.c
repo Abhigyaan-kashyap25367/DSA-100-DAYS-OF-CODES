b8348621f79260d07b167be7b193ce0f6f8cb517
Abhigyaan-kashyap25367 <ABHIGYAAN.25367@stu.upes.ac.in>
2026-04-06 20:10:20 +0530

    Add files via upload

---
+++ Q_64.c
@@ -0,0 +1,104 @@
+//Problem: Perform BFS from a given source using queue.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+    int* visited;
+};
+
+struct Queue {
+    int items[100];
+    int front;
+    int rear;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = malloc(vertices * sizeof(struct Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+}
+
+struct Queue* createQueue() {
+    struct Queue* q = malloc(sizeof(struct Queue));
+    q->front = -1;
+    q->rear = -1;
+    return q;
+}
+
+int isEmpty(struct Queue* q) {
+    return q->rear == -1;
+}
+
+void enqueue(struct Queue* q, int value) {
+    if (q->front == -1) q->front = 0;
+    q->rear++;
+    q->items[q->rear] = value;
+}
+
+int dequeue(struct Queue* q) {
+    int item = q->items[q->front];
+    q->front++;
+    if (q->front > q->rear) q->front = q->rear = -1;
+    return item;
+}
+
+void BFS(struct Graph* graph, int startVertex) {
+    struct Queue* q = createQueue();
+    graph->visited[startVertex] = 1;
+    enqueue(q, startVertex);
+
+    while (!isEmpty(q)) {
+        int currentVertex = dequeue(q);
+        printf("%d ", currentVertex);
+
+        struct Node* temp = graph->adjLists[currentVertex];
+        while (temp) {
+            int adjVertex = temp->data;
+            if (graph->visited[adjVertex] == 0) {
+                graph->visited[adjVertex] = 1;
+                enqueue(q, adjVertex);
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    int n, edges, s, src, dest;
+    scanf("%d", &n);
+    struct Graph* graph = createGraph(n);
+    scanf("%d", &edges);
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src, dest);
+    }
+    scanf("%d", &s);
+    BFS(graph, s);
+    return 0;
+}
