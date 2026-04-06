130c68a111d35848759152341968b85b7e53e75a
Abhigyaan-kashyap25367 <ABHIGYAAN.25367@stu.upes.ac.in>
2026-04-06 20:10:00 +0530

    Add files via upload

---
+++ Q_63.c
@@ -0,0 +1,76 @@
+//Problem: Perform DFS starting from a given source vertex using recursion.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+    int* visited;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = malloc(vertices * sizeof(struct Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+}
+
+void DFS(struct Graph* graph, int vertex) {
+    struct Node* adjList = graph->adjLists[vertex];
+    struct Node* temp = adjList;
+
+    graph->visited[vertex] = 1;
+    printf("%d ", vertex);
+
+    while (temp != NULL) {
+        int connectedVertex = temp->data;
+
+        if (graph->visited[connectedVertex] == 0) {
+            DFS(graph, connectedVertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, edges, s, src, dest;
+
+    scanf("%d", &n);
+    struct Graph* graph = createGraph(n);
+
+    scanf("%d", &edges);
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src, dest);
+    }
+
+    scanf("%d", &s);
+
+    DFS(graph, s);
+
+    return 0;
+}
