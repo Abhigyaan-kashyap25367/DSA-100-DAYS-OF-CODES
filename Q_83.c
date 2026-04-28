f8222f7fd60e046f145d06d851132038e30ab84b
Abhigyaan-kashyap25367 <ABHIGYAAN.25367@stu.upes.ac.in>
2026-04-28 09:20:09 +0530

    Add files via upload

---
+++ Q_83.c
@@ -0,0 +1,37 @@
+//Problem: Implement Selection Sort - Implement the algorithm.
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int n, i;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    selectionSort(arr, n);
+
+    for (i = 0; i < n; i++) {
+        printf("%d%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
